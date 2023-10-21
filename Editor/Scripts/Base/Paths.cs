0ab4ddb96ba394c2cac9426d0b71133497463600
Илья Мещеряков <qpkiraqp@gmail.com>
2023-10-21 14:55:54 +0300

    Подправить пути

--- Editor/Scripts/Base/Paths.cs
+++ Editor/Scripts/Base/Paths.cs
@@ -2,8 +2,8 @@
 {
     public class Paths
     {
-        public const string Uss = "Assets/Module/InteractiveEditor/Editor/Scripts/Base/InteractiveEditor.uss";
-        public const string Uxml = "Assets/Module/InteractiveEditor/Editor/Scripts/Base/InteractiveEditor.uxml";
-        public const string NodeViewUxml = "Assets/Module/InteractiveEditor/Editor/Scripts/Base/NodeView.uxml";
+        public const string Uss = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/InteractiveEditor.uss";
+        public const string Uxml = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/InteractiveEditor.uxml";
+        public const string NodeViewUxml = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/NodeView.uxml";
     }
 }
