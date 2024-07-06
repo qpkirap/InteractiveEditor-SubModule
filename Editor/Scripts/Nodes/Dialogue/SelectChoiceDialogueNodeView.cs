5f8a3f82220a7eb509f1ac10ba863e890f0dc9a3
Илья Мещеряков <qpkiraqp@gmail.com>
2024-07-06 22:37:27 +0300

    WIP

--- Editor/Scripts/Nodes/Dialogue/SelectChoiceDialogueNodeView.cs
+++ Editor/Scripts/Nodes/Dialogue/SelectChoiceDialogueNodeView.cs
@@ -13,7 +13,7 @@ namespace Module.InteractiveEditor.Editor
         
         public override Type InputPortType => typeof(bool);
         public override Type OutputPortType => typeof(AnswerChoiceDialogueNodeView);
-        public override Port.Capacity InputPortCapacity => Port.Capacity.Single;
+        public override Port.Capacity InputPortCapacity => Port.Capacity.Multi;
         public override Port.Capacity OutputPortCapacity => Port.Capacity.Multi;
     }
 }
