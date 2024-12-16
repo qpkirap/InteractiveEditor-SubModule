f046ed6a6d7cab900337f5819f776a477e864457
Илья Мещеряков <qpkiraqp@gmail.com>
2024-12-16 09:25:09 +0300

    Add editor

---
+++ Editor/Scripts/Nodes/Dialogue/Editors/SelectChoiceDialogueConditionsEditor.cs
@@ -0,0 +1,10 @@
+﻿using Module.InteractiveEditor.Configs;
+using UnityEditor;
+
+namespace Module.InteractiveEditor.Editor
+{
+    [CustomEditor(typeof(SelectChoiceDialogueConditionsNode))]
+    public class SelectChoiceDialogueConditionsEditor : BaseDialogueEditor
+    {
+    }
+}
