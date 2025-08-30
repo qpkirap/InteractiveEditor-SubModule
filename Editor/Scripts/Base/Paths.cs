a0df3193174babd6ada3d88d044e8c80ce1c3bb3
Илья Мещеряков <qpkiraqp@gmail.com>
2025-08-30 23:06:13 +0300

    wip

--- Editor/Scripts/Base/Paths.cs
+++ Editor/Scripts/Base/Paths.cs
@@ -5,10 +5,5 @@
         public const string Uss = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/InteractiveEditor.uss";
         public const string Uxml = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/InteractiveEditor.uxml";
         public const string NodeViewUxml = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/NodeView.uxml";
-        
-        // Note: Old UXML/USS paths removed as we've migrated to Odin Inspector
-        // Only keep paths for remaining UIElements-based editors
-        public const string ImagesWindowUxml = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/Episodes/ImageEditor/ImageWindowEditor.uxml";
-        public const string ImagesWindowUss = "Assets/6 - Submodules/InteractiveEditor/Editor/Scripts/Base/Episodes/ImageEditor/ImageWindowEditor.uss";
     }
 }
